30f5f184c2eea9986885231bfb5f61abd3e1fd69
Pragya0115 <109800495+Pragya0115@users.noreply.github.com>
2023-06-15 19:01:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const inputBox=document.getElementById("input-box");
+const listContainer=document.getElementById("list-container");
+
+function addTask()
+{
+    if(inputBox.value=== '')
+    {
+        alert("Write something");
+    }
+    else{
+        let li= document.createElement("li");
+        li.innerHTML=inputBox.ariaValueText;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e)
+{
+    if(e.target.tagName==="LI")
+    {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN")
+    {
+        e.target.parentElemnet.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showList()
+{
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showList();
